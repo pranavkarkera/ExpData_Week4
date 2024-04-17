84bcbc89cedad2ac59a840b200747b3c621ef0e2
Pranav Karkera <157125124+pranavkarkera@users.noreply.github.com>
2024-04-18 02:31:43 +0530

    Add files via upload

---
+++ plot1.R
@@ -0,0 +1,14 @@
+library("data.table")
+
+SCC <- data.table::as.data.table(x = readRDS(file = "Source_Classification_Code.rds"))
+NEI <- data.table::as.data.table(x = readRDS(file = "summarySCC_PM25.rds"))
+
+# Prevents histogram from printing in scientific notation
+NEI[, Emissions := lapply(.SD, as.numeric), .SDcols = c("Emissions")]
+
+totalNEI <- NEI[, lapply(.SD, sum, na.rm = TRUE), .SDcols = c("Emissions"), by = year]
+
+barplot(totalNEI[, Emissions]
+        , names = totalNEI[, year]
+        , xlab = "Years", ylab = "Emissions"
+        , main = "Emissions over the Years")
